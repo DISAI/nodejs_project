157e8b57caa89b48ef8a7208a35012535254aeeb
stephone <84649688@qq.com>
2018-12-13 00:11:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- 将来 new 的Vue实例，会控制这个 元素中的所有内容 -->
+  <!-- Vue 实例所控制的这个元素区域，就是我们的 V  -->
+  <div id="app">
+    <!-- 顶部 Header 区域 -->
+    <mt-header fixed title="商城·Vue项目"></mt-header>
+    <!-- 中间的 路由 router-view 区域 -->
+    <!-- 底部 Tabbar 区域 -->
+    <nav class="mui-bar mui-bar-tab">
+      <a class="mui-tab-item mui-active" href="#tabbar">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-chat">
+        <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+        <span class="mui-tab-label">消息</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-contact">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">通讯录</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-map">
+        <span class="mui-icon mui-icon-gear"></span>
+        <span class="mui-tab-label">设置</span>
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import { Header } from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import './style/mui/css/mui.min.css'
+Vue.component(Header.name, Header)
+Vue.config.productionTip = false
+
+/* 主入口 */
+/* eslint-disable no-new */
+// 2. 创建一个Vue的实例
+// 当我们导入包之后，在浏览器的内存中，就多了一个 Vue 构造函数
+//  注意：我们 new 出来的这个 vm 对象，就是我们 MVVM中的 VM调度者
+new Vue({
+  el: '#app', // 表示，当前我们 new 的这个 Vue 实例，要控制页面上的哪个区域
+  router,
+  components: { App },
+  template: '<App/>'
+})
